b041b43bea3c02b0532bd250a36596da3c4f4aad
Blaire Baker <bakerblaire@gmail.com>
2021-01-11 15:46:02 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+ function findMatching(arrayOfDrivers, name) {
+     return arrayOfDrivers.filter(e => e.toLowerCase() === name.toLowerCase())
+ }
+
+ function fuzzyMatch(arrayOfDrivers, letters) {
+     let length = letters.length;
+     return arrayOfDrivers.filter(name => name.slice(0, length) === letters)
+ }
+
+ function matchName(arrayOfDrivers, string) {
+    return arrayOfDrivers.filter(object => object.name === string)
+ }
